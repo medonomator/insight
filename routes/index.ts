21d42a7a73ad040ae78688b296aa0f1689e43ba7
Your Name <you@example.com>
2019-04-20 23:08:25 +0300

    base settings hapi

--- routes/index.ts
+++ routes/index.ts
@@ -1,19 +1,35 @@
 import * as Hapi from 'hapi';
 import * as Joi from 'joi';
+import { getTemplateMainPage } from '../controllers'
 // import docs from '../docs';
 
 const users: Hapi.ServerRoute[] = [
   {
     method: 'GET',
-    path: '/users',
+    path: '/',
     // handler: controller.get,
-    handler: () => {
-      console.log('users');
+    handler: (req, h) => {
+      console.log('This is home page!');
 
       return {
-        users: [1, 2, 3]
+        message: 'This is home page!'
       }
     },
+    // options: {
+    //   // ...docs.getDoc,
+    //   auth: {
+    //     strategy: 'user',
+    //     access: {
+    //       scope: ['action: read, target: users', 'admin'],
+    //     },
+    //   },
+    // },
+  },
+  {
+    method: 'GET',
+    path: '/{mainPages}',
+    // handler: controller.get,
+    handler: getTemplateMainPage,
     // options: {
     //   // ...docs.getDoc,
     //   auth: {
--- server.ts
+++ server.ts
@@ -4,102 +4,92 @@ import * as Inert from 'inert';
 import * as Vision from 'vision';
 import * as Handlebars from 'handlebars';
 // import * as HapiSwagger from 'hapi-swagger';
+
 import { setUpconnection, expenseScope } from './database';
 // routes
 import users from './routes';
 
-
-
-
-
-
 setUpconnection();
-(async () => {
-  const opa = await expenseScope.find({ _id: '5cbb473261a0891ecc1cd245' });
-
-  opa.update({ food: 100000 })
-
-})();
-
-
-
-// const logger = Pino();
-
-// export default class Server {
-//   private port: string;
-//   private _server: Hapi.Server;
-//   private hemera: Hemera;
-//   private isInitSucceed = false;
-//   constructor(port: string = '5000') {
-//     this.port = port;
-//   }
-//   private async addPlugins() {
-//     await this._server.register([
-//       Inert,
-//       Vision,
-//       // Handlebars,
-//       // AuthBearer,
-//       // JWT,
-//       // {
-//       //   plugin: HapiSwagger,
-//       //   options: this.swaggerOptions,
-//       // },
-//     ]);
-//   }
-//   public async init() {
-//     try {
-//       process.on('unhandledRejection', (error: Error) => {
-//         logger.error(error);
-//         process.exit(1);
-//       });
-//       this._server = new Hapi.Server({
-//         port: this.port,
-//         routes: {
-//           cors: { origin: ['*'] },
-//           validate: {
-//             failAction: async (req, h, err) => {
-
-//               console.log('test validate');
-
-
-//               throw err;
-//             },
-//           },
-//         },
-//       });
-
-
-//       await this.addPlugins();
-
-//       this._server.route([...users,]);
-//       this.isInitSucceed = true;
-//     } catch (error) {
-//       logger.error(error);
-//     }
-//   }
-//   public async start() {
-//     if (this.isInitSucceed) {
-//       await this._server.start();
-//       logger.info(`Server running at ${this.server.info.uri}`);
-//     }
-//   }
-//   get server() {
-//     return this._server;
-//   }
-// }
-
-// const server = new Server(process.env.PORT || '5000');
-
 // (async () => {
-//   await server.init();
-//   server.start();
+//   const opa = await expenseScope.update({ _id: '5cbb473261a0891ecc1cd245' });
+
+//   opa.update({ food: 100000 })
 // })();
 
-// process.on('unhandledRejection', (error: Error) => {
-//   console.error(error.message);
-//   console.error(error.stack);
-// });
+const logger = Pino();
 
-// process.on('uncaughtException', (error: Error) => {
-//   console.error(`uncaughtException ${error.message}`);
-// });
+export default class Server {
+  private port: string;
+  private _server: Hapi.Server;
+  constructor(port: string = '5000') {
+    this.port = port;
+  }
+  private async addPlugins() {
+    await this._server.register([
+      Inert,
+      Vision,
+      // AuthBearer,
+      // JWT,
+      // {
+      //   plugin: HapiSwagger,
+      //   options: this.swaggerOptions,
+      // },
+    ]);
+  }
+  public async init() {
+    try {
+      process.on('unhandledRejection', (error: Error) => {
+        logger.error(error);
+        process.exit(1);
+      });
+      this._server = new Hapi.Server({
+        port: this.port,
+        routes: {
+          cors: { origin: ['*'] },
+          validate: {
+            failAction: async (req, h, err) => {
+              logger.error('router validate', err);
+              throw err;
+            },
+          },
+        },
+      });
+      await this.addPlugins();
+
+      this._server.views({
+        engines: {
+          html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'views'
+      });
+
+      this._server.route([...users]);
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+  public async start() {
+    await this._server.start();
+    logger.info(`Server running at ${this.server.info.uri}`);
+  }
+  get server() {
+    return this._server;
+  }
+}
+
+const server = new Server(process.env.PORT || '5000');
+
+(async () => {
+  await server.init();
+  server.start();
+})();
+
+process.on('unhandledRejection', (error: Error) => {
+  console.error(error.message);
+  console.error(error.stack);
+});
+
+process.on('uncaughtException', (error: Error) => {
+  console.error(`uncaughtException ${error.message}`);
+});
