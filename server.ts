12c09bc9b7a6046fe0e3bf0f9d8d9864f1d2a997
Your Name <you@example.com>
2019-08-05 22:38:33 +0300

    change host

--- server.ts
+++ server.ts
@@ -34,7 +34,7 @@ export default class Server {
     try {
       this._server = new Hapi.Server( {
         port: this.port,
-        host: "34.67.102.138",
+        // host: "34.67.102.138",
         routes: {
           cors: { origin: ['*'] },
           validate: {
