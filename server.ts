1856a228362fcdb464d5de1332d898a14a7b0302
John Doe <familyzorins@gmail.com>
2019-12-17 12:09:58 +0300

    change token

--- server.ts
+++ server.ts
@@ -102,12 +102,41 @@ process.on('uncaughtException', (error: Error) => {
   console.error(`uncaughtException ${error.message}`);
 });
 
-const Telegraf = require('telegraf');
+// const Telegraf = require('telegraf');
 
-const bot = new Telegraf('438677012:AAG9bOGJjWlugHvqr5wl5XURwQecV9m3LXc');
+// const bot = new Telegraf('438677012:AAE0_RuWfqzzaYep-TpuLWJZa54752apY0s');
+// bot.start(ctx => ctx.reply('Welcome'));
+// bot.help(ctx => ctx.reply('Send me a sticker'));
+// bot.on('sticker', ctx => ctx.reply('👍'));
+// bot.hears('hi', ctx => ctx.reply('Hey there'));
+// bot.launch();
 
-bot.start(ctx => ctx.reply('Welcome'));
-bot.help(ctx => ctx.reply('Send me a sticker'));
-bot.on('sticker', ctx => ctx.reply('👍'));
-bot.hears('hi', ctx => ctx.reply('Hey there'));
-bot.launch();
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '438677012:AAE0_RuWfqzzaYep-TpuLWJZa54752apY0s';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token);
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
