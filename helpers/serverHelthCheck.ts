d5004914deba3a7896c633e7d4d6dad3c00025c7
John Doe <familyzorins@gmail.com>
2019-12-18 12:43:32 +0300

    restart only mongo

--- helpers/serverHelthCheck.ts
+++ helpers/serverHelthCheck.ts
@@ -1,4 +1,3 @@
-import os from 'os';
 import axios from 'axios';
 import { spawn } from 'child_process';
 import { logger } from '../helpers/logger';
@@ -6,7 +5,7 @@ import { insertDataToRedis } from '../database/insertDataToRedis';
 import TelegramBot from './telegramBotLauncher';
 import { isEmpty } from 'lodash';
 
-const TIMELIFE_REQUEST = 10000;
+const TIMELIFE_REQUEST = 1000 * 30; // 30 second
 const TIME_UPDATE_CHECKER = 1000 * 60 * 3; //  3 minute
 const BOT_ID = '409011202';
 
@@ -22,10 +21,6 @@ export const serverHelthCheck = () => {
 
       const res = await axios.get('http://familyzorins.tk/admin/aphorisms');
 
-      console.log('=============================');
-      console.log('logging', res.data);
-      console.log('=============================');
-
       if (isEmpty(res.data)) {
         await insertDataToRedis();
         telegramBot.sendMessage(BOT_ID, 'update data in redis');
@@ -33,7 +28,7 @@ export const serverHelthCheck = () => {
       logger.info('Without failures');
     } catch (err) {
       telegramBot.sendMessage(BOT_ID, 'restart docker-compose');
-      const ls = spawn('docker-compose', ['restart']);
+      const ls = spawn('docker-compose', ['restart mongo']);
       ls.stdout.on('data', data => logger.info(`stdout: ${data}`));
       ls.stderr.on('data', data => logger.error(`stderr: ${data}`));
       ls.on('close', code => logger.info(`child process exited with code ${code}`));
