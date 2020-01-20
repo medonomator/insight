aeeef35ab3af5558a52c08d0ede4c621f8827a0b
John Doe <budteschastlivi1@gmail.com>
2020-01-20 22:22:09 +0300

    change text

--- admin-panel/src/helpers/index.js
+++ admin-panel/src/helpers/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
+import { IS_DEVELOPMENT } from '../constant';
 
-export const getBaseUrl = () => {
-  // return 'http://83.166.242.213';
-  return 'http://localhost:5000';
-};
+export const getBaseUrl = () => (IS_DEVELOPMENT ? 'http://localhost:5000' : 'http://83.166.242.213');
 
 export const setAuthorizationToken = token => {
   if (token) {
