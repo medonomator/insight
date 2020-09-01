8e21632062f422221cb84571c389a8087d26ac77
Dmitry Zorin <budteschastlivi1@gmail.com>
2020-09-01 07:42:35 +0300

    remove pass

--- database/seeds/addAdmin.ts
+++ database/seeds/addAdmin.ts
@@ -13,8 +13,8 @@ import { encryptData } from '../../helpers';
       port: 465,
       secure: true,
       auth: {
-        user: process.env.NODEMAILER_AUTH_USER || 'spiritualevolution@yandex.ru',
-        pass: process.env.NODEMAILER_AUTH_PASSWORD || 'generator21',
+        user: process.env.NODEMAILER_AUTH_USER,
+        pass: process.env.NODEMAILER_AUTH_PASSWORD,
       },
     });
 
