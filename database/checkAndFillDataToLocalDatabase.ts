b11a75d67b547f9fcd4c6d8735abc40690c0a715
Dmitry Zorin <budteschastlivi1@gmail.com>
2021-01-08 11:48:01 +0300

    fix fill local database

--- database/checkAndFillDataToLocalDatabase.ts
+++ database/checkAndFillDataToLocalDatabase.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
 import { logger } from "../helpers/logger";
-import { knex } from "./pgConnect";
 import materialsTable from "../tables/materials";
 import { MAIN_HOST_URL } from "../constants";
+import { aphorisms } from '../database/schemas/aphorisms'
 
 export const checkAndFillDataToLocalDatabase = async (): Promise<void> => {
   try {
-    const materials = await knex(materialsTable.table);
+    const aphorismsData = await aphorisms.find();
 
-    if (!materials.length) {
-      const materials = await axios.get(`${MAIN_HOST_URL}v1/admin/materials`);
+    if (!aphorismsData.length) {
+      const aphorismsData = await axios.get(`${MAIN_HOST_URL}v1/admin/materials`);
 
-      materials.data.data.forEach((item) => {
+      aphorismsData.data.data.forEach((item) => {
         item.tags = JSON.stringify(item.tags);
       });
 
-      await knex(materialsTable.table).insert(materials.data.data);
+      await aphorisms.insertMany(aphorismsData.data.data);
 
       logger.info("Insert meterials to local Database complete");
     }
